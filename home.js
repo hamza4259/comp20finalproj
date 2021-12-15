109a83e86366ab6a6363675346e618e2d9070511
dhermov <82853718+dhermov@users.noreply.github.com>
2021-12-14 19:55:30 -0500

    Update home.js

--- home.js
+++ home.js
@@ -82,7 +82,7 @@ function displayCurrentContent(content, contentType, res) {
 }
 
 async function connectAndDisplay(age, gender, workout_type, res) {
-    var t = `<body style = "background: linear-gradient(#002240, white); color: #fff; font-family: 'Lato', sans-serif; text-align: center">`;
+    var t = `<body style = "background: linear-gradient(#047aed, white); color: #fff; font-family: 'Lato', sans-serif; text-align: center">`;
 
     MongoClient.connect(mongoUrl, {useUnifiedTopology: true}, async (err, database) => {
         if (err) {
